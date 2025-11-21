5d376e3653bcf7325a657de5b584f356148a66ad
ryukamish <asuskr25@gmail.com>
2025-11-21 14:32:54 +0530

    add: rendering markdown files beautifully in neovim

---
+++ config/nvim/lua/overlord/plugins/render-markdown.lua
@@ -0,0 +1,14 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+      heading = {
+        enabled = true,
+        width = 'block',
+        icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+        left_pad = 2,
+        right_pad = 4,
+      }
+    }
+}
