a7edbd02ce84d11af4a247da8a5fd6508f722ec0
ryukamish <asuskr25@gmail.com>
2026-03-29 17:17:06 +0530

    add: scrolling to hyprland like niri

---
+++ config/hypr/scripts/toggle-layout.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+current_layout=$(hyprctl getoption general:layout | grep 'str:' | awk '{print $2}')
+
+if [ "$current_layout" = "dwindle" ]; then
+  hyprctl keyword general:layout scrolling
+  sleep 0.3
+  notify-send "Hyprland" "Layout switched to: Scrolling" -t 2000
+else
+  hyprctl keyword general:layout dwindle
+  notify-send "Hyprland" "Layout switched to: Dwindle" -t 2000
+fi
